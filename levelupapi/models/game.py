bd966682aeee709b7162bd191e24f41cc378aefe
Lauren Hanson <lohanson33@gmail.com>
2023-02-07 14:36:00 -0600

    fixtures added

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,7 +4,7 @@ class Game(models.Model):
 
     title = models.CharField(max_length=100)
     maker = models.CharField(max_length=100)
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name='game_types')
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name='label_gametype')
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='game_gamer')
     num_of_players = models.IntegerField()
     skill_level = models.IntegerField()
