46167b4c9eb4685ed8bfb0412011e505c245f04f
Lauren Hanson <lohanson33@gmail.com>
2023-02-13 15:51:05 -0600

    destory added to event_view

--- levelupapi/views/event_view.py
+++ levelupapi/views/event_view.py
@@ -5,15 +5,15 @@ from rest_framework import serializers, status
 from levelupapi.models import Event, Gamer, Game
 
 
-class EventView(ViewSet): 
+class EventView(ViewSet):
 
-    def retrieve(self, request, pk): 
+    def retrieve(self, request, pk):
 
         event = Event.objects.get(pk=pk)
-        serializer = EventSerializer(event) 
-        return Response(serializer.data) 
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
 
-    def list(self, request): 
+    def list(self, request):
 
         events = Event.objects.all()
 
@@ -62,11 +62,16 @@ class EventView(ViewSet):
         event.description = request.data["description"]
         event.time = request.data["time"]
         event.date = request.data["date"]
-    
+
         game = Game.objects.get(pk=request.data["game"])
         event.game = game
         event.save()
 
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        event = Event.objects.get(pk=pk)
+        event.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
